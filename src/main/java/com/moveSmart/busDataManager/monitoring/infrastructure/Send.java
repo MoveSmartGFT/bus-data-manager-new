1c600a850ea8f4f46dae794f60618e6631456397
loui <loui@gft.com>
2024-11-13 12:51:27 +0100

    -Added ampq dependency to pom -Created receiver and sender poc classes for queues

---
+++ src/main/java/com/moveSmart/busDataManager/monitoring/infrastructure/Send.java
@@ -0,0 +1,28 @@
+package com.moveSmart.busDataManager.monitoring.infrastructure;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+
+        String uri = "amqps://niduuxqx:64q4XK7jFyAF_cigL6W2SXwevqZsx0hJ@kangaroo.rmq.cloudamqp.com/niduuxqx";
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUri(uri);
+        factory.setConnectionTimeout(30000);
+
+        try (Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
