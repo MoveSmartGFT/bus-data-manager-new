b99a486c0823505a02b7b257fc934b6c71fa72bd
degm <degm@gft.com>
2025-01-16 09:21:07 +0100

    fix recv commit

--- src/main/java/com/movesmart/busdatamanager/monitoring/infrastructure/Recv.java
+++ src/main/java/com/movesmart/busdatamanager/monitoring/infrastructure/Recv.java
@@ -20,7 +20,7 @@ public class Recv {
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        DefaultConsumer consumer = new DefaultConsumer(channel) {
+        /* DefaultConsumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(
                     String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
@@ -30,5 +30,6 @@ public class Recv {
         };
 
         channel.basicConsume(QUEUE_NAME, true, consumer);
+         */
     }
 }
